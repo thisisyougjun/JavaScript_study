7503fad67c4a7269b01945a3b36da2bfab3c6e79
thisisyoungjun <122899@naver.com>
2022-01-05 21:46:08 +0900

    22-01-05

--- Login/main.js
+++ Login/main.js
@@ -1,6 +1,8 @@
 const loginFrom =document.querySelector("#login-form");
 const loginInput =document.querySelector("#login-form input");
 
+const logoutFrom =document.querySelector("#logout-from");
+
 const greeting =document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME = "hidden";
@@ -12,13 +14,19 @@ function onLoginBtnClick(event){
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY,username);
     paintGreetings(username)
+    logoutFrom.classList.remove(HIDDEN_CLASSNAME);//로그아웃 hidden 클래스 제거 
+}
+
+function LogoutBtnClick(event){
+    localStorage.clear() //로컬 스토리지 데이터 삭제
 }
 
 loginFrom.addEventListener("submit",onLoginBtnClick); //()사용하지 않으면 즉시 호출 되지 않고 필요할때만 호출된다①
 //onLoginBtnClick 함수는 내가 호출 한게 아니라 브라우저에서 호출 해주는것이고
 //나는 자리만 마련해준것이다
+logoutFrom.addEventListener("submit",LogoutBtnClick);
 
-function  paintGreetings(username){
+function paintGreetings(username){
     greeting.innerText =`Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -31,6 +39,9 @@ if (savedUsername === null){
 }else {
    paintGreetings(savedUsername);
 }
+
+
+
 
 
 
