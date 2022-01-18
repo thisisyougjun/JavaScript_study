963d55fceeec40c9a78c4018a8ea6874134f3c2f
thisisyoungjun <122899@naver.com>
2022-01-18 20:13:03 +0900

    1-18

---
+++ Nomacoders/Login/js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("h2#clock");
+
+function sayHello(){
+    const date = new Date();
+    const hours    = String(date.  getHours()).padStart(2,"0");
+    const minutes  = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText=`${hours}:${minutes}:${seconds}`
+
+}
+
+console.log(Date())
+sayHello();
+setInterval(sayHello,1000);
+
+
